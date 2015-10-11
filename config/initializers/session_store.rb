377b19a9df13aa9b428816e1b5265b67ba55b60d
Robbie Cooper <cooperra@users.noreply.github.com>
2015-10-11 07:18:22 -0400

    Rename app to 'Useless Key Generator'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_useless-key-distributor_session'
+Rails.application.config.session_store :cookie_store, key: '_useless-key-generator_session'
